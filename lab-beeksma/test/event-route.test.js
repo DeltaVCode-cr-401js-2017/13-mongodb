bd0f0207a95a2ca989981b88059287b01c70e2ca
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-30 15:15:21 -0500

    tests for GET

---
+++ lab-beeksma/test/event-route.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const app = require('../server');
+const request = require('supertest')(app);
+const { expect } = require('chai');
+const Note = require('../model/event');
+
+describe('event routes', function () {
+  describe('GET /api/event', function (){
+    describe('with an invalid id', function (){
+      it('should return 404', function (){
+        return request
+        .get('/api/event/missing')
+        .expect(404);
+      });
+    });
+    describe('with a valid id', function (){
+      before(function (){
+        return new Note ({ title: 'get me'})
+        .save()
+        .then(event => this.testEvent = event);
+      });
+      after(function () {
+        Note.remove({});
+      });
+      it('should return a note', function (){
+        return request
+          .get(`/api/event/${this.testEvent._id}`)
+          .expect(200)
+          .expect(res => {
+            expect(res.body.title).to.equal(this.testEvent.title);
+          });
+      });
+    });
+  });
+});
