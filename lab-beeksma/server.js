0bfa4d74daa05d85287e7b28431ecba2b6346480
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-30 14:18:33 -0500

    error-middleware

--- lab-beeksma/server.js
+++ lab-beeksma/server.js
@@ -7,6 +7,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(require('./lib/error-middleware'));
+
 if (!module.parent){
   app.listen(PORT, () => debug(`Listening on ${PORT}`));
 }
