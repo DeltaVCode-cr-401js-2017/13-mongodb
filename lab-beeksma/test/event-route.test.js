c18990a10d97ec113ddd56d6d696dc47930e3d99
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-30 15:30:33 -0500

    post tests ;

--- lab-beeksma/test/event-route.test.js
+++ lab-beeksma/test/event-route.test.js
@@ -6,6 +6,7 @@ const { expect } = require('chai');
 const Event = require('../model/event');
 
 describe('event routes', function () {
+
   describe('GET /api/event', function (){
     describe('with an invalid id', function (){
       it('should return 404', function (){
@@ -29,6 +30,29 @@ describe('event routes', function () {
           .expect(200)
           .expect(res => {
             expect(res.body.title).to.equal(this.testEvent.title);
+          });
+      });
+    });
+  });
+
+  describe('POST /api/note', function(){
+    describe('with no body', function(){
+      return request
+        .post('api/event')
+        .expect(400);
+    });
+    describe('with a valid body', function(){
+      after(function () {
+        Event.remove({});
+      });
+      it('should return a note', function (){
+        return request
+          .post('/api/event')
+          .send({title: 'test event'})
+          .expect(200)
+          .expect(res => {
+            expect(res.body.title).to.equal('test event');
+            expect(res.body.created).to.not.be.undefined;
           });
       });
     });
