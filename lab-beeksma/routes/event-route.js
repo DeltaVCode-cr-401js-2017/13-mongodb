e1d0e2fec40206c3e12fc9b5cb1304a7bcdf740f
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-30 16:24:30 -0500

    put request updates item

--- lab-beeksma/routes/event-route.js
+++ lab-beeksma/routes/event-route.js
@@ -20,3 +20,27 @@ router.post('/api/event', jsonParser, function (req,res,next){
     .then(item => res.json(item))
     .catch(next);
 });
+
+router.put('/api/event/:id', jsonParser, function(req,res,next){
+  debug('PUT /api/event');
+  if(!req.body){
+    return Promise.reject(res.sendStatus(400));
+  }
+  Event.findById(req.params.id, function(err,item){
+    if (err) {
+      res.sendStatus(404);
+    }
+    else {
+      for (var prop in Event.schema.paths) {
+        if ((prop !== '_id') && (prop !== '__v')) {
+          if (req.body[prop] !== undefined) {
+            item[prop] = req.body[prop];
+          }
+        }
+      }
+      item.save()
+      .then(item => res.json(item))
+      .catch(next);
+    }
+  });
+});
